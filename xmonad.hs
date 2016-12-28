dc6459e50eb2683cee7b0cf79cafdc361258af90
rig0rmortis <admin@rig0rmortis.com>
2016-12-27 22:28:35 -0500

    Bottom xmobar, xmonad changes for workspaces, some color stuff

--- xmonad.hs
+++ xmonad.hs
@@ -1,11 +1,15 @@
 import XMonad
+import System.IO
 import XMonad.Config.Desktop
 import XMonad.Layout.NoBorders
+import XMonad.Layout.Spacing
 import Graphics.X11.ExtraTypes.XF86
 import XMonad.Util.EZConfig
 import XMonad.Util.Paste
+import XMonad.Util.Run(spawnPipe)
 import XMonad.Hooks.ManageDocks
-import qualified XMonad.Hooks.ICCCMFocus as ICCCMFocus
+import XMonad.Hooks.DynamicLog
+-- import qualified XMonad.Hooks.ICCCMFocus as ICCCMFocus
 
 baseConfig = desktopConfig
 
@@ -25,12 +29,45 @@ myManageHook = composeAll
   [ className =? "Steam"    --> doFloat
   , className =? "albert"   --> doFloat]
 
-main = xmonad $ baseConfig
-  { terminal = "urxvt"
+myWorkspaces =
+  [
+    "1:general",
+    "2:web",
+    "3:code",
+    "4:code",
+    "5:communication",
+    "6:data",
+    "7:torrent",
+    "8:games",
+    "9:containers"
+  ]
+
+xmobarTitleColor = "#F44336"
+
+xmobarCurrentWorkspaceColor = "#4CAF50"
+
+xmobarHiddenColor = "#EEEEEE"
+
+main = do
+  xmbproc <- spawnPipe "xmobar ~/.xmobarrc-bottom"
+  xmonad $ baseConfig {
+    terminal = "urxvt"
   , modMask	= mod1Mask
-  , borderWidth = 1
-  , layoutHook = myLayoutHook
-  -- , manageHook = myManageHook <+> manageDocks
+  , borderWidth = 3
+  , normalBorderColor = "#313131"
+  , focusedBorderColor = "#4A235A"
+  , workspaces = myWorkspaces
+  , layoutHook = smartSpacing 10 $ myLayoutHook
   , manageHook = manageHook baseConfig <+> manageDocks
-  , logHook = ICCCMFocus.takeTopFocus
-	} `additionalKeys` myKeys
+  --, logHook = ICCCMFocus.takeTopFocus
+  , logHook = dynamicLogWithPP $ xmobarPP {
+      ppOutput = hPutStrLn xmbproc
+      , ppTitle = xmobarColor xmobarTitleColor "" . shorten 100
+      , ppCurrent = xmobarColor xmobarCurrentWorkspaceColor ""
+      , ppHidden = xmobarColor xmobarHiddenColor ""
+      , ppHiddenNoWindows = xmobarColor xmobarHiddenColor ""
+      , ppOrder = \(ws:_:t:_) -> [ws,t]
+      , ppSep = "    "
+      , ppWsSep = "  "
+      }
+  } `additionalKeys` myKeys
